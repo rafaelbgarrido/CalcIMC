9f135b885eddeb5f6434ef271fb87f9ec56c9b9f
Rafael Barrios Garrido <81597206+rafaelbgarrido@users.noreply.github.com>
2021-07-13 20:06:16 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function (evento) {
-    evento.preventDefault();
-    const inputPeso = evento.target.querySelector('#peso');
-    const inputAltura = evento.target.querySelector('#altura');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
 
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
